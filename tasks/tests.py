cc71a49954ad115d959239a29aaf67adb81953e0
Bruno Vinícius <bvmcardoso@gmail.com>
2021-09-17 17:31:33 -0300

    Testes básicos de criação de tag e task_list.

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,2 +1,16 @@
 from django.test import TestCase
+from .models import Tag, TaskList, Task
 
+class TestTag(TestCase):
+
+    def test_tag_creation(self):
+        tag = Tag.objects.create(name="Tag 4")
+        last_tag = Tag.objects.last()
+        self.assertEqual(last_tag.name, "Tag 4")
+
+    def test_task_list_creation(self):
+        task_list = TaskList.objects.create(name="Walk the dog")
+        last_task_list = TaskList.objects.last()
+        self.assertEqual(last_task_list.name, "Walk the dog")
+    
+  
